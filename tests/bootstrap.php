3a6469e8b5d607bd1aa5c5073cf25463e6fb37b5
Andrew Shell <andrew@andrewshell.org>
2026-04-07 16:46:44 -0500

    Restructuring repo to simplify copying plugin to svn

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -29,7 +29,7 @@ require_once "{$_tests_dir}/includes/functions.php";
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( __DIR__, 1 ) . '/rsscloud.php';
+	require dirname( __DIR__, 1 ) . '/rsscloud/rsscloud.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
